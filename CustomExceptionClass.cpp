f0490a01c1a97647fb1060498d44ef804905eab2
Nandhini15 <nandhininandhu94@gmail.com>
2018-11-04 09:56:56 -0500

    Update CustomExceptionClass.cpp

--- CustomExceptionClass.cpp
+++ CustomExceptionClass.cpp
@@ -6,7 +6,7 @@ class myexception: public exception
 {
 // virtual keyword.. not typecast of derived class to base class is done
 // always retrieve corresponding method in respec derived class
-//throw keyword plays a active role
+//const throw keyword - exception in virtual class.. only then the derived class may override the base class and throw its corres method.
  virtual const char* what() const throw()
   {
     return "My exception happened";
