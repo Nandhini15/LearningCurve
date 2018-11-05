2dc035e186f9e5d59397e86288851eb5e3fd6d2f
Nandhini15 <nandhininandhu94@gmail.com>
2018-11-05 14:19:33 -0500

    Create sum.lastdigit.cpp

---
+++ sum.lastdigit.cpp
@@ -0,0 +1,16 @@
+import java.io.*;
+import  java.util.*;
+
+// Read only region start
+class UserMainCode
+{
+
+	public int addLastDigits(int input1,int input2){
+		// Read only region end
+		// Write code here...
+		input1 = input1%10;
+		input2 = input2%10;
+		return input1+input2;
+	}
+	
+}
