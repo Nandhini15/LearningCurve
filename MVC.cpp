826613f94129b5b9322fb9d2705e7352ae95cdf9
Nandhini15 <nandhininandhu94@gmail.com>
2018-11-08 13:57:10 -0500

    Create MVC.cpp

---
+++ MVC.cpp
@@ -0,0 +1,11 @@
+//Model is directly responsible for handling data
+//Model component accesses MYSQL database
+//also called data objects
+//Does not depend upon view or controller
+//the Model does not care how its data can be displayed or when to be updated.
+//The data changes in the Model will generally be published through some event handlers. 
+//Model is responsible for data get and set
+//View - interact with model to retrieve data as the output
+//Controller - all the business logics works here.
+//controller takes as input both model and view.handles request and response. eg.onload what happens, post get methods etchttps://www.bogotobogo.com/DesignPatterns/mvc_model_view_controller_pattern.php
+//https://www.bogotobogo.com/DesignPatterns/mvc_model_view_controller_pattern.php
