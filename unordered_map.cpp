72f9aa7774ef9bf9577a31ad5e698112b58a9a98
Nandhini15 <nandhininandhu94@gmail.com>
2018-11-09 20:51:18 -0500

    Create unordered_map.cpp

---
+++ unordered_map.cpp
@@ -0,0 +1,25 @@
+#include <iostream> 
+#include <unordered_map>
+#include <iterator>
+using namespace std;
+//unique keys
+//no sorted order
+int main()
+{
+    unordered_map<string, double> a;
+    a["root1"] = 3.14;
+    a["root2"] = 4.14;
+    a["root3"] = 5.14;
+    a["root4"] = 6.14;
+    a.insert(make_pair("root5",8.90));
+    string key = "root1";
+    if(a.find(key) == a.end()){
+        cout<<key<<"not found"<<endl;
+    }else{
+        cout<<key<<" found"<<endl;
+    }
+    unordered_map<string, double>::iterator i;
+    for(i = a.begin(); i != a.end(); ++i)
+    cout<<i->first<<" "<<i->second<<endl;
+    return 0;
+}
